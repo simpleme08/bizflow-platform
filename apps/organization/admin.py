5c5f2ff457719756ef937bf5a01898900c6d8ab6
simpleme08 <141198336+simpleme08@users.noreply.github.com>
2026-09-10 12:57:40 +0800

    Expose subscription state in organization admin

--- apps/organization/admin.py
+++ apps/organization/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 
 from .models import CostCenter, Department, EmploymentType, Organization, OrganizationMembership, Position
 
-admin.site.register(Organization)
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'plan_code', 'subscription_status', 'is_active', 'overage_rate')
+    list_filter = ('plan_code', 'subscription_status', 'is_active')
+    search_fields = ('name', 'slug', 'billing_customer_id')
+
+
 admin.site.register(OrganizationMembership)
 admin.site.register(Department)
 admin.site.register(Position)
