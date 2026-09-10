0b54d7b5cc4ca1d82e4f9410e366397ba1b3e328
simpleme08 <141198336+simpleme08@users.noreply.github.com>
2026-09-10 23:45:28 +0800

    Configure media storage for attendance proof photos

--- config/settings.py
+++ config/settings.py
@@ -57,6 +57,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 STORAGES = {'staticfiles': {'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage'}}
 
 SECURE_SSL_REDIRECT = not DEBUG
