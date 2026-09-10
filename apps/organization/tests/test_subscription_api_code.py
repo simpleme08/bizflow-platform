3f219890541aebaf87eb4c4b5f0df33dcb7cdf43
simpleme08 <141198336+simpleme08@users.noreply.github.com>
2026-09-10 12:27:32 +0800

    Test plan code response

---
+++ apps/organization/tests/test_subscription_api_code.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from apps.organization.models import Organization, OrganizationMembership
+
+
+class SubscriptionCodeApiTests(TestCase):
+    def test_plan_code_is_exposed(self):
+        org = Organization.objects.create(name='Acme', slug='acme', plan_code=Organization.Plan.STARTER)
+        user = get_user_model().objects.create_user(username='owner')
+        OrganizationMembership.objects.create(organization=org, user=user, role=OrganizationMembership.Role.OWNER)
+        client = Client(); client.force_login(user)
+        self.assertEqual(client.get('/api/subscription/').json()['plan']['code'], 'STARTER')
