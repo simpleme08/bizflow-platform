e444578b1f2278759d4813b9537a5b594abd55fc
simpleme08 <141198336+simpleme08@users.noreply.github.com>
2026-09-10 10:13:46 +0800

    Align payroll profile migration with BaseModel UUID identity

--- apps/payroll/migrations/0003_payrollprofile_statutory.py
+++ apps/payroll/migrations/0003_payrollprofile_statutory.py
@@ -1,17 +1,15 @@
+import uuid
 from django.db import migrations, models
 import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-    dependencies = [
-        ('payroll', '0002_payrollperiod_organization'),
-    ]
-
+    dependencies = [('payroll', '0002_payrollperiod_organization')]
     operations = [
         migrations.CreateModel(
             name='PayrollProfile',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
                 ('sss_number', models.CharField(blank=True, max_length=20)),
@@ -21,31 +19,10 @@ class Migration(migrations.Migration):
                 ('minimum_wage_earner', models.BooleanField(default=False)),
                 ('employee', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='payroll_profile', to='employees.employee')),
             ],
-            options={'abstract': False},
         ),
-        migrations.AddField(
-            model_name='payrollperiod',
-            name='frequency',
-            field=models.CharField(choices=[('SEMI_MONTHLY', 'Semi-monthly'), ('MONTHLY', 'Monthly')], default='SEMI_MONTHLY', max_length=20),
-        ),
-        migrations.AddField(
-            model_name='payrollrecord',
-            name='sss_employee',
-            field=models.DecimalField(decimal_places=2, default=0, max_digits=12),
-        ),
-        migrations.AddField(
-            model_name='payrollrecord',
-            name='philhealth_employee',
-            field=models.DecimalField(decimal_places=2, default=0, max_digits=12),
-        ),
-        migrations.AddField(
-            model_name='payrollrecord',
-            name='pagibig_employee',
-            field=models.DecimalField(decimal_places=2, default=0, max_digits=12),
-        ),
-        migrations.AddField(
-            model_name='payrollrecord',
-            name='withholding_tax',
-            field=models.DecimalField(decimal_places=2, default=0, max_digits=12),
-        ),
+        migrations.AddField(model_name='payrollperiod', name='frequency', field=models.CharField(choices=[('SEMI_MONTHLY', 'Semi-monthly'), ('MONTHLY', 'Monthly')], default='SEMI_MONTHLY', max_length=20)),
+        migrations.AddField(model_name='payrollrecord', name='sss_employee', field=models.DecimalField(decimal_places=2, default=0, max_digits=12)),
+        migrations.AddField(model_name='payrollrecord', name='philhealth_employee', field=models.DecimalField(decimal_places=2, default=0, max_digits=12)),
+        migrations.AddField(model_name='payrollrecord', name='pagibig_employee', field=models.DecimalField(decimal_places=2, default=0, max_digits=12)),
+        migrations.AddField(model_name='payrollrecord', name='withholding_tax', field=models.DecimalField(decimal_places=2, default=0, max_digits=12)),
     ]
