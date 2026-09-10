801f01e8391a251ee055231254ab2e095dec3c7a
simpleme08 <141198336+simpleme08@users.noreply.github.com>
2026-09-10 10:21:04 +0800

    Fix holiday migration UUID default

--- apps/payroll/migrations/0006_payrollholiday.py
+++ apps/payroll/migrations/0006_payrollholiday.py
@@ -1,3 +1,4 @@
+import uuid
 import django.db.models.deletion
 from django.db import migrations, models
 
@@ -9,7 +10,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='PayrollHoliday',
             fields=[
-                ('id', models.UUIDField(editable=False, primary_key=True, serialize=False)),
+                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
                 ('holiday_date', models.DateField()),
