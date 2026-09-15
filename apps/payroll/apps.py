c147a4f8e3f5152278296f490be48d18c0afb5bb
simpleme08 <paulalexiscruzoffc@gmail.com>
2026-09-15 13:28:43 +0800

    Register payroll confidence signals

--- apps/payroll/apps.py
+++ apps/payroll/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PayrollConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.payroll'
+
+    def ready(self):
+        from . import signals  # noqa: F401
