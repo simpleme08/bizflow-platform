a5cc4775d39aa5caaf2db339dd68a38c6a4e8257
simpleme08 <paulalexiscruzoffc@gmail.com>
2026-09-16 13:00:37 +0800

    test: use database-aware testcase for production seed guard

--- apps/attendance/test_demo_seed_security.py
+++ apps/attendance/test_demo_seed_security.py
@@ -1,9 +1,9 @@
 from django.core import management
 from django.core.management.base import CommandError
-from django.test import SimpleTestCase, override_settings
+from django.test import TestCase, override_settings
 
 
-class DemoAttendanceSeedSecurityTests(SimpleTestCase):
+class DemoAttendanceSeedSecurityTests(TestCase):
     @override_settings(ENVIRONMENT='production')
     def test_demo_attendance_seed_is_blocked_in_production(self):
         with self.assertRaises(CommandError):
